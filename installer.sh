dcdbb8fce93be291be2e19ba2251a61d1fd55351
Emilien Jegou <emilien.jegou@epitech.eu>
2019-05-16 19:50:47 +0200

    installer updated

--- installer.sh
+++ installer.sh
@@ -8,4 +8,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+set -e
+
+apt update && apt install npm node -y
+
 cat ./tools/viabird.service | sed "s/{{ PATH }}/$(echo $DIR | sed 's/\//\\\//g')/" > /etc/systemd/system/viabird.service
